a328c22b6da94c9b37f0dac788005d25cb930cda
Shoe-Pac <mesic.mirza07@gmail.com>
2025-04-09 02:36:59 +0200

    fix(backend): bcrypt deployment producrion compilation fix

--- apps/backend/src/graphql/resolvers/UserResolver.ts
+++ apps/backend/src/graphql/resolvers/UserResolver.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcryptjs'
 import { Arg, Mutation, Query, Resolver } from 'type-graphql'
 
 import { removeModelsFromAws } from '../../database/aws'
