a553baf10a53025200507b1e326af1c73450e7b7
Shoe-Pac <mesic.mirza07@gmail.com>
2025-04-15 11:33:24 +0200

    cors bugfix - missing s in https

--- apps/backend/src/app/app.ts
+++ apps/backend/src/app/app.ts
@@ -15,8 +15,8 @@ app.register(cors, {
   origin: [
     'http://localhost:4001',
     'https://web-object-renderer.onrender.com',
-    'http://web-object-renderer.online',
-    'http://web-object-render.online',
+    'https://web-object-renderer.online',
+    'https://web-object-render.online',
     'https://wor-0ovu.onrender.com', //Static Site instead of Web Service deployed on Render
     'https://b7fc-188-129-80-141.ngrok-free.app', //ngrok tunnel for testing and development purpose
     'https://uptimerobot.com' //UptimeRobot pinging service for keeping backend always alive
