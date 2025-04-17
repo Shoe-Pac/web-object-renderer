5ad7f3e1c6fe24ef5c836ae4cb97eaec7dba97d2
Shoe-Pac <mesic.mirza07@gmail.com>
2025-04-17 13:17:06 +0200

    maintainance(test): frontend e2e landing and login page initial test update

--- apps/frontend-e2e/src/login.spec.ts
+++ apps/frontend-e2e/src/login.spec.ts
@@ -1,15 +1,15 @@
 import { expect, test } from '@playwright/test'
 
-test('Login page has title', async ({ page }) => {
+test('Landing page loads', async ({ page }) => {
   await page.goto('/')
 
-  // Expect h1 to contain a substring.
-  expect(await page.locator('h1').innerText()).toContain('Login')
+  await expect(page).toHaveTitle('Web Object Renderer') //Page title check
+  await expect(page.getByText('WOR')).toBeVisible() //Page text check
 })
 
-test('Login page loads', async ({ page }) => {
-  await page.goto('/')
+test('Login page has title', async ({ page }) => {
+  await page.goto('/login')
 
-  await expect(page).toHaveTitle('Web Object Renderer') // Provjera title-a stranice
-  await expect(page.getByText('WOR')).toBeVisible() // Provjera teksta na stranici
+  // Expect h1 to contain a substring.
+  await expect(page.locator('h1')).toContainText('Login')
 })
